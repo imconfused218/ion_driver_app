3312a193f27c7d8a54f0d3ab4d21f09a73b79f35
Matt Hoenecke <matt@menu.me>
2016-02-22 15:29:01 -0800

    adds some css

--- www/js/app.js
+++ www/js/app.js
@@ -96,7 +96,7 @@ AssignmentsCtrl.prototype.acceptAssignment = function () {
 
   this.agentService.assignmentAction(assignmentId, 'accept/').then(function(results){
     self.agentService.getAssignments();
-    this.selectedAssignment = undefined;
+    self.selectedAssignment = undefined;
     self.$location.path('/activeAssignment')
   })
 };
